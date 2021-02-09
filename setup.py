d6825b21c9d413100c738e32496a74b920fd0150
Maximiliano <maximiliano.besoain@gmail.com>
2021-02-09 18:07:01 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+	name = "MySQL",
+	version = '1.0',
+	description = 'MySQL but it reconnects',
+	author = 'shinbyh',
+	packages = find_packages()
+)
